347d654773709b96133768371f3b0c25ac65220c
Beatriz German <beatriz.german@kairosds.com>
2020-02-11 12:05:38 +0100

    Add Onchange method

--- src/app/data-binding/child/child.component.ts
+++ src/app/data-binding/child/child.component.ts
@@ -1,11 +1,30 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output
+} from "@angular/core";
 
 @Component({
   selector: "app-child",
   templateUrl: "./child.component.html",
   styleUrls: ["./child.component.css"]
 })
-export class ChildComponent implements OnInit {
+// export class ChildComponent implements OnInit {
+//   constructor() {}
+//   @Input() message: string; // abro una interfaz donde le permito a otro componente pasarme info
+//   @Output() reply = new EventEmitter<string>();
+
+//   ngOnInit() {
+//     // this.reply.emit('me has dicho '+ this.message );
+//   }
+//   onClick() {
+//     this.reply.emit(`me has dicho ${this.message}`);
+//   }
+// }
+export class ChildComponent implements OnInit, OnChanges {
   constructor() {}
   @Input() message: string; // abro una interfaz donde le permito a otro componente pasarme info
   @Output() reply = new EventEmitter<string>();
@@ -13,7 +32,7 @@ export class ChildComponent implements OnInit {
   ngOnInit() {
     // this.reply.emit('me has dicho '+ this.message );
   }
-  onClick() {
+  ngOnChanges() {
     this.reply.emit(`me has dicho ${this.message}`);
   }
 }
