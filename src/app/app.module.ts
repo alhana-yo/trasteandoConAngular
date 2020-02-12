aa7af637535be634d1ec76c35a88adec213754ad
Beatriz German <beatriz.german@kairosds.com>
2020-02-12 13:43:41 +0100

    Add nested routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule, Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { AuthService } from "./auth/auth.service";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthService } from './auth/auth.service';
+import { LoginComponent } from './auth/login/login.component';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+import { LayoutsModule } from './layouts/layouts.module';
+import { SimpleComponent } from './layouts/simple/simple.component';
 // ya no hace falta importar estos módulos porque los cargamos mediante el lazy loading
 // con esto conseguimos reducir el tamaño del bundle principal. Porque de esta manera, los módulos cargados lazy, no están cargados desde el principio
 // import { DataBindingModule } from "./data-binding/data-binding.module";
 // import { DirectivasModule } from "./directivas/directivas.module";
 
 const config = {
-  api: "http://back"
+  api: 'http://back'
 };
 
 // const ROUTES: Routes = [
@@ -26,29 +30,62 @@ const config = {
 // ];
 // En angular 9
 
+// const ROUTES: Routes = [
+//   { path: "", redirectTo: "directivas", pathMatch: "full" },
+//   {
+//     path: "directivas",
+//     loadChildren: () =>
+//       import("./directivas/directivas.module").then(m => m.DirectivasModule)
+//   },
+//   {
+//     path: "data-binding",
+//     canActivate: [AuthService],
+//     loadChildren: () =>
+//       import("./data-binding/data-binding.module").then(
+//         m => m.DataBindingModule
+//       )
+//   },
+//   { path: "**", redirectTo: "directivas" }
+// ];
+
 const ROUTES: Routes = [
-  { path: "", redirectTo: "directivas", pathMatch: "full" },
   {
-    path: "directivas",
-    loadChildren: () =>
-      import("./directivas/directivas.module").then(m => m.DirectivasModule)
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: 'directivas',
+        loadChildren: () =>
+          import('./directivas/directivas.module').then(m => m.DirectivasModule)
+      },
+      {
+        path: 'data-binding',
+        canActivate: [AuthService],
+        loadChildren: () =>
+          import('./data-binding/data-binding.module').then(
+            m => m.DataBindingModule
+          )
+      }
+    ]
   },
   {
-    path: "data-binding",
-    canActivate: [AuthService],
-    loadChildren: () =>
-      import("./data-binding/data-binding.module").then(
-        m => m.DataBindingModule
-      )
+    path: 'login',
+    component: SimpleComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      }
+    ]
   },
-  { path: "**", redirectTo: "directivas" }
+  { path: '**', redirectTo: 'directivas' }
 ];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(ROUTES)],
-  providers: [{ provide: "config", useValue: config }],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES), LayoutsModule],
+  providers: [{ provide: 'config', useValue: config }],
 
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AppLayoutComponent } from "./app-layout/app-layout.component";
+import { SimpleComponent } from "./simple/simple.component";
+
+@NgModule({
+  declarations: [SimpleComponent, AppLayoutComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [AppLayoutComponent, SimpleComponent]
+})
+export class LayoutsModule {}
