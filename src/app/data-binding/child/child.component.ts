57efd52c8fb3ffc26f0c275f030b9f207ce8ba95
Beatriz German <beatriz.german@kairosds.com>
2020-02-12 12:40:43 +0100

    Add canDeactive example

--- src/app/data-binding/child/child.component.ts
+++ src/app/data-binding/child/child.component.ts
@@ -7,18 +7,23 @@ import {
   Output
 } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { CanComponentDeactivate } from "src/app/auth/confirm-service.service";
 
 @Component({
   selector: "app-child",
   templateUrl: "./child.component.html",
   styleUrls: ["./child.component.css"]
 })
-export class ChildComponent implements OnInit, OnChanges {
+export class ChildComponent
+  implements OnInit, OnChanges, CanComponentDeactivate {
   constructor(private activatedRoute: ActivatedRoute) {}
+
   @Input() message: string; // abro una interfaz donde le permito a otro componente pasarme info
   @Output() reply = new EventEmitter<string>();
   id: string;
   param: string;
+  salir: boolean;
+
   ngOnInit() {
     // Es mejor trabajar con el subscribe antes que con el snapshot,
     // porque con el snapshot tenemos problemas a la hora de recuperar
@@ -33,9 +38,25 @@ export class ChildComponent implements OnInit, OnChanges {
     this.activatedRoute.queryParams.subscribe(queryParams => {
       this.param = queryParams.param;
     });
+
+    this.salir = false;
   }
   // Este método es para estar atentos a los cambios que hay dentro de los inputs
   ngOnChanges() {
     this.reply.emit(`me has dicho ${this.message}`);
+  }
+
+  canDeactivate(): boolean {
+    if (this.salir) {
+      console.log("en el canDeactivate", this.salir);
+      return true;
+    } else {
+      return confirm("Seguro que quieres salir sin guardar?");
+    }
+  }
+
+  onClick() {
+    this.salir = true;
+    console.log(this.salir);
   }
 }
--- src/app/data-binding/data-binding.module.ts
+++ src/app/data-binding/data-binding.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ConfirmService } from "../auth/confirm-service.service";
 import { ChildComponent } from "./child/child.component";
 import { FatherComponent } from "./father/father.component";
 
 // partimos de cero, porque vamos a cargar este módulo lazy
 const ROUTES: Routes = [
   { path: "", component: FatherComponent },
-  { path: ":curso", component: ChildComponent }
+  { path: ":curso", component: ChildComponent, canDeactivate: [ConfirmService] }
 ];
 @NgModule({
   declarations: [FatherComponent, ChildComponent],
