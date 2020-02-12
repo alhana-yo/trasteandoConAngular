3f0cfedf9b6c98a57fceafd34e8bf608cbe20d6f
Beatriz German <beatriz.german@kairosds.com>
2020-02-12 11:08:45 +0100

    Improve the recoverage of info in params

--- src/app/data-binding/child/child.component.ts
+++ src/app/data-binding/child/child.component.ts
@@ -20,8 +20,19 @@ export class ChildComponent implements OnInit, OnChanges {
   id: string;
   param: string;
   ngOnInit() {
-    this.id = this.activatedRoute.snapshot.params.curso;
-    this.param = this.activatedRoute.snapshot.queryParams.param;
+    // Es mejor trabajar con el subscribe antes que con el snapshot,
+    // porque con el snapshot tenemos problemas a la hora de recuperar
+    // información
+
+    // this.id = this.activatedRoute.snapshot.params.curso;
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params.curso;
+    });
+
+    // this.param = this.activatedRoute.snapshot.queryParams.param;
+    this.activatedRoute.queryParams.subscribe(queryParams => {
+      this.param = queryParams.param;
+    });
   }
   // Este método es para estar atentos a los cambios que hay dentro de los inputs
   ngOnChanges() {
