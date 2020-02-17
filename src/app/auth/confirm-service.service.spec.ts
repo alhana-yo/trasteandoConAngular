e530d43c33a487973b810ad1b5bfb91f9c49bbb2
Beatriz German <beatriz.german@kairosds.com>
2020-02-17 13:17:06 +0100

    Add some tests

--- src/app/auth/confirm-service.service.spec.ts
+++ src/app/auth/confirm-service.service.spec.ts
@@ -1,16 +1,9 @@
-import { TestBed } from '@angular/core/testing';
-
-import { ConfirmServiceService } from './confirm-service.service';
 
 describe('ConfirmServiceService', () => {
-  let service: ConfirmServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ConfirmServiceService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
   });
 });
---
+++ src/app/users/users-proxy.service.spec.ts
@@ -0,0 +1,33 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { FAKE_USERS } from './users-fake.spec';
+import { UsersProxyService } from './users-proxy.service';
+
+
+
+describe('UsersProxyService', () => {
+  // let service: UsersProxyService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    // service = TestBed.inject(UsersProxyService);
+  });
+
+  it('should be created', () => {
+    const service: UsersProxyService = TestBed.inject(UsersProxyService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should call to server to get users', () => {
+    const service: UsersProxyService = TestBed.inject(UsersProxyService);
+    const httpMock = TestBed.inject(HttpTestingController);
+    service.getUsers().subscribe(users => expect(users).toBe(FAKE_USERS));
+    // De esta forma nos estamos protegiendo si alguien modifica esa url
+    const request = httpMock.expectOne('http://localhost:3001/api/public/users');
+    expect(request.request.method).toEqual('GET');
+    request.flush(FAKE_USERS);
+    httpMock.verify();
+  });
+});
